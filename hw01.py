2628e67e486bce17d06ef4c20d7960032df3ebcc
Stephanie Noelle <sndiffotsangneza@smith.edu>
2026-09-22 23:49:03 -0400

    I did it

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: (Stephanie Noelle Ndiffotsang Neza)
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,14 +45,34 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
-
+    x=27
+    y=1
+    a=1.5
+    b=7
+    c=-1
+    result1 = (3*x - 9*y) / ( 2*a * (b - c))
+    
+    print("Part 1: x = 27")
+    print("Part 1: y = 1")
+    print("Part 1: a = 1.5")
+    print("Part 1: b = 7")
+    print("Part 1: c = -1")
+    print("Part 1: result =",result1)
+    
     # End of Part 1 ----------------------
 
 
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
-
+    x=5
+    y=-3
+    result2= (x**2) * (y**4)
+    
+    print("Part 2: x = 5")
+    print("Part 2: y = -3")
+    print("Part 2: result =",result2)
+    
     # End of Part 2 ----------------------
 
 
@@ -60,14 +80,23 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
-
+    a=100 #(representing the total number of treats)
+    b=13 # (the total number of dogs)  
+    result3 = a//b
+    
+    print("Part 3: a = 100")
+    print("Part 3: b = 13")
+    print("Part 3: result =",result3)
+    
     # End of Part 3 ----------------------
 
 
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
-
+    result4 = a%b
+    print("Part 4: result =",result4)
+    
     # End of Part 4 ----------------------
 
 if __name__ == "__main__":
